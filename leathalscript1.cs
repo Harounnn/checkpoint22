6d50690855fc9b71f9a65e2c2feebe95e2a697c8
Harounnn <67702192+Harounnn@users.noreply.github.com>
2020-10-17 15:27:09 +0100

    Add files via upload

---
+++ leathalscript1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leathalscript1 : MonoBehaviour
+{
+    public float Health;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            collision.gameObject.GetComponent<HealthManager>().TakeHealth(Health);
+        }
+    }
+}
